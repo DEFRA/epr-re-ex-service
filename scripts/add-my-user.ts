2a124d1d2a740019b8d297d1511d736e1eb607c8
Carles Andres <carlos.andres.c@gmail.com>
2025-12-08 21:37:24 +0000

    WIP Improved messaging

--- scripts/add-my-user.ts
+++ scripts/add-my-user.ts
@@ -1,24 +1,27 @@
-import { ORG_1_ID } from "./constants";
-import { fetchBackend } from "./fetch-backend.ts";
-
-(async (): Promise<void> => {
+import { ORG_1_ID } from './constants'
+import { fetchBackend } from './fetch-backend.ts'
+;(async (): Promise<void> => {
   const payload = {
     organisation: {
       users: [
         {
-          email: "saul.goodman@bettercall.com",
-          fullName: "Saul Goodman",
+          email: 'saul.goodman@bettercall.com',
+          fullName: 'Saul Goodman',
           isInitialUser: true,
-          roles: ["standard_user"],
-        },
-      ],
-    },
-  };
+          roles: ['standard_user']
+        }
+      ]
+    }
+  }
 
   const data = await fetchBackend(
     `/organisations/${ORG_1_ID}`,
-    "PATCH",
-    payload,
-  );
-  console.log("data.organisations.users", data.organisation.users);
-})();
+    'PATCH',
+    payload
+  )
+
+  console.log(
+    '\n\nIf this list does not include Mickey Mouse, then it has failed to update'
+  )
+  console.log('data.organisations.users', data.organisation.users)
+})()
