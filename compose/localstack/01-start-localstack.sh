e7573cd360b6a8a70336cd62f92b2ea4fdd5a117
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-18 11:14:48 +0000

    parallelise bucket/queue creation

--- compose/localstack/01-start-localstack.sh
+++ compose/localstack/01-start-localstack.sh
@@ -7,22 +7,24 @@ export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
-echo "[INIT SCRIPT] Creating buckets" >&2
+echo "[INIT SCRIPT] Creating buckets and queues" >&2
 
-aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine
-aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-summary-logs
-aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-public-register
-
-echo "[INIT SCRIPT] Creating queues" >&2
+aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine &
+aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-summary-logs &
+aws --endpoint-url=http://localhost:4566 s3 mb s3://re-ex-public-register &
 
 # queues
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-clamav-results
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-download-requests
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}"
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name epr_backend_commands_dlq
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name epr_backend_commands --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:epr_backend_commands_dlq\\\",\\\"maxReceiveCount\\\":\\\"3\\\"}\"}"
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-clamav-results &
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-download-requests &
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}" &
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name epr_backend_commands_dlq &
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name epr_backend_commands --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:epr_backend_commands_dlq\\\",\\\"maxReceiveCount\\\":\\\"3\\\"}\"}" &
 
 # test harness
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name mock-clamav
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name mock-clamav &
+
+wait
+
+echo "[INIT SCRIPT] Configuring bucket notifications" >&2
+
 aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cdp-uploader-quarantine --notification-configuration '{"QueueConfigurations": [{"QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav","Events": ["s3:ObjectCreated:*"]}]}'
-aws --endpoint-url=http://localhost:4566 sqs create-queue --region $AWS_REGION --queue-name cdp-uploader-download-requests
